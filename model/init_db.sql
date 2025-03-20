714ee35405aa588084c55e5f661578baf85fb91b
alessiadod <alessia.michetti@gmail.com>
2025-03-20 14:15:43 +0100

    correct working main

--- model/init_db.sql
+++ model/init_db.sql
@@ -2,16 +2,45 @@ DROP TABLE IF EXISTS `interactions`;
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `items`;
 
---
--- Drop Tables
---
-SET foreign_key_checks = 0;
-DROP TABLE if exists users;
-SET foreign_key_checks = 1;
+CREATE TABLE `users`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) UNIQUE NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `phonenumber` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
 
--- the order of the tables is important in the migration file because of the foreign key constraints
-
-
+CREATE TABLE `items`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `created_at` TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT  CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `title` VARCHAR(255) NOT NULL,
+    `image` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `category` ENUM(
+        'tools',
+        'outdoor',
+        'kitchenware',
+        'cleaning',
+        'electronics',
+        'sports',
+        'furniture',
+        'events',
+        'childrens',
+        'seasonal',
+        'crafts',
+        'media',
+        'vehicles',
+        'misc'
+    ) NOT NULL,
+    `owner_id` INT NOT NULL,
+    `status` ENUM('available', 'unavailable', '') NOT NULL,
+    `latitude` INT NULL,
+    `longitude` INT NULL,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE CASCADE 
+);
 
 CREATE TABLE `interactions` (
     `id` INT NOT NULL AUTO_INCREMENT,
